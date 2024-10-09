fae84531cfad46446b757f90af0502618c6a8f89
Palanisamy M <146609846+Palanisamy2@users.noreply.github.com>
2024-10-09 20:06:21 +0530

    Add files via upload

---
+++ portfolio-palanisamy/assert/javascript/main.js
@@ -0,0 +1,8 @@
+const skillBtn = document.getElementById('Btn');
+
+let skillPage = document.getElementById('SkillContainer')
+
+skillBtn.onclick = function() {
+    skillPage.style.display = 'flex';
+}
+
